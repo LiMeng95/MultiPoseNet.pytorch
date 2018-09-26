826e84e4250428aced95e6764f1014f95cafec8c
Meng Li <thulimeng@gmail.com>
2018-09-26 22:04:08 +0800

    delete _upsample_add() in posenet.py

--- network/posenet.py
+++ network/posenet.py
@@ -166,30 +166,6 @@ class poseNet(nn.Module):
 
         # self.freeze_bn()  # from retinanet
 
-    def _upsample_add(self, x, y):
-        '''Upsample and add two feature maps.
-
-        Args:
-          x: (Variable) top feature map to be upsampled.
-          y: (Variable) lateral feature map.
-
-        Returns:
-          (Variable) added feature map.
-
-        Note in PyTorch, when input size is odd, the upsampled feature map
-        with `F.upsample(..., scale_factor=2, mode='nearest')`
-        maybe not equal to the lateral feature map size.
-
-        e.g.
-        original input size: [N,_,15,15] ->
-        conv2d feature map size: [N,_,8,8] ->
-        upsampled feature map size: [N,_,16,16]
-
-        So we choose bilinear upsample which supports arbitrary output sizes.
-        '''
-        _, _, H, W = y.size()
-        return F.upsample(x, size=(H, W), mode='True', align_corners=False) + y
-
     def forward(self, x):
 
         # if self.training:
